e388ab17a170f076ec9316f3e9ed34be7d3edb7b
Dane-LeBlanc <dane.leblanc@gmail.com>
2022-04-12 21:19:12 -0600

    completed insertion sort

--- insertion.js
+++ insertion.js
@@ -1,3 +1,23 @@
-function insertionSort() {}
+function insertionSort(arr) {
+  let min;
+  let idx;
+  for (let i = 0; i < arr.length - 1; i++) {
+    min = arr[i];
+    idx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < min) {
+        min = arr[j];
+        idx = j;
+      }
+    }
+    if (idx !== i) {
+      arr[idx] = arr[i];
+      arr[i] = min;
+    }
+  }
+  return arr;
+}
+
+console.log(insertionSort([4, 20, 12, 10, 7, 9]));
 
 module.exports = insertionSort;
--- quick.js
+++ quick.js
@@ -3,7 +3,9 @@ pivot accepts an array, starting index, and ending index
 You can assume the pivot is always the first element
 */
 
-function pivot(){}
+function pivot(arr, start = 0, end = arr.length - 1){
+    
+}
 
 /*
 quickSort accepts an array, left index, and right index
